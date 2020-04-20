fcaa2566d934eafb458b7954de7a03fe0cd3909a
amansharma-dev <amansharma.11@hotmail.com>
2020-04-20 10:46:08 +0530

    commit

--- app/src/main/java/com/example/step2_projectdve/Adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/step2_projectdve/Adapter/RecyclerViewAdapter.java
@@ -1,35 +1,63 @@
 package com.example.step2_projectdve.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.step2_projectdve.Model.Product;
+import com.example.step2_projectdve.R;
+
+import java.util.List;
+
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Product> productList;
+
+    public RecyclerViewAdapter(Context context, List<Product> productList) {
+        this.context = context;
+        this.productList = productList;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(context).inflate(R.layout.product_row,parent,false);
+
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
-
+        Product product = productList.get(position);
+        holder.productName.setText(product.getName());
+        holder.productLink.setText(product.getLink());
     }
 
 
     @Override
     public int getItemCount() {
-        return 0;
+        return productList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView productName;
+        public Button productLink;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            productName = itemView.findViewById(R.id.productName_textView);
+            productLink = itemView.findViewById(R.id.productLink_Button);
         }
     }
 }
--- app/src/main/java/com/example/step2_projectdve/MainActivity.java
+++ app/src/main/java/com/example/step2_projectdve/MainActivity.java
@@ -1,25 +1,37 @@
 package com.example.step2_projectdve;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.step2_projectdve.Adapter.RecyclerViewAdapter;
 import com.example.step2_projectdve.Data.DatabaseHandler;
 import com.example.step2_projectdve.Model.Product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private RecyclerView recyclerView;
+    private ArrayList<Product> productArrayList;
     public static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.main_recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        productArrayList = new ArrayList<>();
 
         DatabaseHandler databaseHandler = new DatabaseHandler(MainActivity.this);
-
+//
 //        databaseHandler.addProduct(new Product("Umano","https://umano.com/"));
 //        databaseHandler.addProduct(new Product("Beard Head","https://beardhead.com/"));
 //        databaseHandler.addProduct(new Product("Ice Shaker","https://www.iceshaker.com/"));
@@ -32,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 //        databaseHandler.addProduct(new Product("R. Riveter","https://www.rriveter.com/"));
 //        databaseHandler.addProduct(new Product("Slyde Handboards","https://www.slydehandboards.com/"));
 //        databaseHandler.addProduct(new Product("Grip Clean","https://www.gripclean.com/"));
-
+//
 
         //delete single product
 //        databaseHandler.deleteSingleProduct(databaseHandler.getSingleProduct(13));
@@ -46,10 +58,14 @@ public class MainActivity extends AppCompatActivity {
         List<Product> productList = databaseHandler.getAllProduct();
         for (Product product : productList){
             Log.d(TAG, "onCreate: "+"NAME: "+product.getName()+", LINK: "+product.getLink());
+            productArrayList.add(product);
         }
 
         //get count
         Log.d(TAG, "onCreate: total count:: "+databaseHandler.getCount());
 
+        //setup adapter
+        recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this,productArrayList);
+        recyclerView.setAdapter(recyclerViewAdapter);
     }
 }
