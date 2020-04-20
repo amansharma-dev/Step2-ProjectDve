8b685d3942a743942530343a5e4301ee42f4d733
amansharma-dev <amansharma.11@hotmail.com>
2020-04-20 11:04:44 +0530

    commit

--- app/src/main/java/com/example/step2_projectdve/Adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/step2_projectdve/Adapter/RecyclerViewAdapter.java
@@ -1,6 +1,9 @@
 package com.example.step2_projectdve.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +12,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.step2_projectdve.Model.Product;
@@ -19,6 +23,7 @@ import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
+    public static final String TAG = "RecyclerViewAdapter";
     private Context context;
     private List<Product> productList;
 
@@ -49,15 +54,30 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return productList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView productName;
         public Button productLink;
+        public CardView cardView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             productName = itemView.findViewById(R.id.productName_textView);
             productLink = itemView.findViewById(R.id.productLink_Button);
+            cardView = itemView.findViewById(R.id.cardProduct_cardView);
+            cardView.setCardBackgroundColor(context.getResources().getColor(R.color.colorCardBackground));
+
+            productLink.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            Log.d(TAG, "onClick: position "+position);
+            Uri uri = Uri.parse(productLink.getText().toString());
+            Log.d(TAG, "onClick: "+productLink.getText().toString());
+            Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+            context.startActivity(intent);
         }
     }
 }
